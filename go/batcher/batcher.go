c379a0f45d48356c66283c6b11793dddf83dcacb
Alexey Kovalev <kovalev.al@phystech.edu>
2024-04-17 03:09:11 +0300

    fix go

--- go/batcher/batcher.go
+++ go/batcher/batcher.go
@@ -3,39 +3,50 @@
 package batcher
 
 import (
+	"sync"
 	"sync/atomic"
 
 	"gitlab.com/slon/shad-go/batcher/slow"
 )
 
 type Batcher struct {
-    s       *slow.Value
-    last    interface{}
-    barrier chan struct{}
-    running atomic.Bool
-    gen     atomic.Uint64
+	s        *slow.Value
+	last     interface{}
+	barrier  chan struct{}
+	barrier_ chan struct{}
+	running  atomic.Bool
+	waiting  atomic.Bool
+	wg       sync.WaitGroup
 }
 
 func NewBatcher(v *slow.Value) *Batcher {
-    return &Batcher{s: v, last: nil, barrier: make(chan struct{})}
+	return &Batcher{s: v, barrier: make(chan struct{}), barrier_: make(chan struct{})}
 }
 
 func (b *Batcher) Load() interface{} {
-    if b.running.CompareAndSwap(false, true) {
-        b.last = b.s.Load()
-        b.running.Store(false)
-        close(b.barrier)
-        b.barrier = make(chan struct{})
-        return b.last
-    }
+	if b.waiting.Load() {
+		<-b.barrier_
+	}
+	b.waiting.Store(true)
 
-    select {
-    case <-b.barrier:
-        return b.last
-    }
-}
+	if b.running.CompareAndSwap(false, true) {
+		defer func() {
+			close(b.barrier)
+			b.barrier = make(chan struct{})
+			b.wg.Wait()
+			b.running.Store(false)
+			b.waiting.Store(false)
+			close(b.barrier_)
+			b.barrier_ = make(chan struct{})
+		}()
+		b.last = b.s.Load()
+		return b.last
+	}
 
-func (b *Batcher) Store(v interface{}) {
-    b.gen.Add(1)
-    b.s.Store(v)
+	b.wg.Add(1)
+	defer func() {
+		b.wg.Done()
+	}()
+	<-b.barrier
+	return b.last
 }
