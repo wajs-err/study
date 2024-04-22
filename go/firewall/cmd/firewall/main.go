c8fa815773acd709b32286eda9e81dba348d970a
Alexey Kovalev <kovalev.al@phystech.edu>
2024-04-22 05:03:19 +0300

    add some go

--- go/firewall/cmd/firewall/main.go
+++ go/firewall/cmd/firewall/main.go
@@ -2,6 +2,257 @@
 
 package main
 
-func main() {
+import (
+	"bytes"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
 
+	"gopkg.in/yaml.v2"
+)
+
+type EndpointConfig struct {
+	Endpoint               string   `yaml:"endpoint"`
+	ForbiddenUserAgents    []string `yaml:"forbidden_user_agents"`
+	ForbiddenHeaders       []string `yaml:"forbidden_headers"`
+	RequiredHeaders        []string `yaml:"required_headers"`
+	MaxRequestLengthBytes  *int64   `yaml:"max_request_length_bytes"`
+	MaxResponseLengthBytes *int64   `yaml:"max_response_length_bytes"`
+	ForbiddenResponseCodes []int    `yaml:"forbidden_response_codes"`
+	ForbiddenRequestRe     []string `yaml:"forbidden_request_re"`
+	ForbiddenResponseRe    []string `yaml:"forbidden_response_re"`
+}
+
+type configInternal struct {
+	Rules []EndpointConfig `yaml:"rules"`
+}
+
+type Config map[string]EndpointConfig
+
+func ParseConfig(path string) Config {
+	yamlConfig, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	configArray := configInternal{}
+	err = yaml.Unmarshal(yamlConfig, &configArray)
+	if err != nil {
+		panic(err)
+	}
+
+	config := Config{}
+	for _, v := range configArray.Rules {
+		config[v.Endpoint] = v
+	}
+	return config
+}
+
+func ParseArgs() (*url.URL, string, string) {
+	serviceAddr := flag.String("service-addr", "http://localhost:8080", "address of service")
+	conf := flag.String("conf", "./firewall/configs/example.yaml", "path to config")
+	addr := flag.String("addr", "localhost:8000", "address for firewall")
+	flag.Parse()
+
+	url, err := url.Parse(*serviceAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	return url, *conf, *addr
+}
+
+var _ http.RoundTripper = (*Firewall)(nil)
+
+type Firewall struct {
+	ServiceURL *url.URL
+	Config     Config
+}
+
+func DefaultForbiddenReponse() *http.Response {
+	return &http.Response{
+		StatusCode: http.StatusForbidden,
+		Body:       io.NopCloser(bytes.NewBufferString("Forbidden")),
+	}
+}
+
+func (f *Firewall) CheckRequestLength(req *http.Request) *http.Response {
+	m := f.Config[req.URL.Path].MaxRequestLengthBytes
+	if m != nil && req.ContentLength > *m {
+		return DefaultForbiddenReponse()
+	}
+	return nil
+}
+
+func (f *Firewall) CheckForbiddenRequestUserAgents(req *http.Request) *http.Response {
+	agents := req.Header.Values("User-Agent")
+	for _, re := range f.Config[req.URL.Path].ForbiddenUserAgents {
+		for _, a := range agents {
+			match, err := regexp.Match(re, []byte(a))
+			if err != nil {
+				panic(err)
+			}
+			if match {
+				return DefaultForbiddenReponse()
+			}
+		}
+	}
+	return nil
+}
+
+func (f *Firewall) CheckRequestForbiddenHeaders(req *http.Request) *http.Response {
+	w := &strings.Builder{}
+	req.Header.Write(w)
+	for _, re := range f.Config[req.URL.Path].ForbiddenHeaders {
+		match, err := regexp.Match(re, []byte(w.String()))
+		if err != nil {
+			panic(err)
+		}
+		if match {
+			return DefaultForbiddenReponse()
+		}
+	}
+	return nil
+}
+
+func (f *Firewall) CheckRequestRequiredHeaders(req *http.Request) *http.Response {
+	w := &strings.Builder{}
+	req.Header.Write(w)
+	for _, re := range f.Config[req.URL.Path].RequiredHeaders {
+		match, err := regexp.Match(re, []byte(w.String()))
+		if err != nil {
+			panic(err)
+		}
+		if !match {
+			return DefaultForbiddenReponse()
+		}
+	}
+	return nil
+}
+
+func (f *Firewall) CheckRequestBody(req *http.Request) *http.Response {
+	b, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	req.Body = io.NopCloser(bytes.NewBuffer(b))
+	for _, re := range f.Config[req.URL.Path].ForbiddenRequestRe {
+		match, err := regexp.Match(re, b)
+		if err != nil {
+			panic(err)
+		}
+		if match {
+			return DefaultForbiddenReponse()
+		}
+	}
+
+	return nil
+}
+
+func (f *Firewall) CheckResponseLength(resp *http.Response, url string) *http.Response {
+	m := f.Config[url].MaxResponseLengthBytes
+	if m != nil && resp.ContentLength > *m {
+		return DefaultForbiddenReponse()
+	}
+	return nil
+}
+
+func (f *Firewall) CheckForbiddenResponseCodes(resp *http.Response, url string) *http.Response {
+	for _, c := range f.Config[url].ForbiddenResponseCodes {
+		if resp.StatusCode == c {
+			return DefaultForbiddenReponse()
+		}
+	}
+	return nil
+}
+
+func (f *Firewall) CheckResponseBody(resp *http.Response, url string) *http.Response {
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body = io.NopCloser(bytes.NewBuffer(b))
+	for _, re := range f.Config[url].ForbiddenResponseRe {
+		match, err := regexp.Match(re, b)
+		if err != nil {
+			panic(err)
+		}
+		if match {
+			return DefaultForbiddenReponse()
+		}
+	}
+
+	return nil
+}
+
+func (f *Firewall) RoundTrip(req *http.Request) (*http.Response, error) {
+	if _, ok := f.Config[req.URL.Path]; ok {
+		if resp := f.CheckRequestLength(req); resp != nil {
+			return resp, nil
+		}
+		if resp := f.CheckRequestForbiddenHeaders(req); resp != nil {
+			return resp, nil
+		}
+		if resp := f.CheckRequestRequiredHeaders(req); resp != nil {
+			return resp, nil
+		}
+		if resp := f.CheckRequestBody(req); resp != nil {
+			return resp, nil
+		}
+		if resp := f.CheckForbiddenRequestUserAgents(req); resp != nil {
+			return resp, nil
+		}
+	}
+
+	url := strings.Clone(req.URL.Path)
+	req.URL = f.ServiceURL
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := f.Config[url]; ok {
+		if updatedResp := f.CheckResponseLength(resp, url); updatedResp != nil {
+			return updatedResp, nil
+		}
+		if updatedResp := f.CheckForbiddenResponseCodes(resp, url); updatedResp != nil {
+			return updatedResp, nil
+		}
+		if updatedResp := f.CheckResponseBody(resp, url); updatedResp != nil {
+			return updatedResp, nil
+		}
+	}
+
+	return resp, nil
+}
+
+func main() {
+	serviceAddr, conf, addr := ParseArgs()
+	config := ParseConfig(conf)
+	c := http.Client{Transport: &Firewall{ServiceURL: serviceAddr, Config: config}}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		req.RequestURI = ""
+		resp, err := c.Do(req)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(resp.StatusCode)
+		for h, vs := range resp.Header {
+			for _, v := range vs {
+				w.Header().Add(h, v)
+			}
+		}
+		defer resp.Body.Close()
+		io.Copy(w, resp.Body)
+	})
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
--- go/olympics/main.go
+++ go/olympics/main.go
@@ -2,6 +2,240 @@
 
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+)
 
+type MedalsInfo struct {
+	Gold   int `json:"gold"`
+	Silver int `json:"silver"`
+	Bronze int `json:"bronze"`
+	Total  int `json:"total"`
+}
+
+func (mi *MedalsInfo) Add(other MedalsInfo) {
+	mi.Gold += other.Gold
+	mi.Silver += other.Silver
+	mi.Bronze += other.Bronze
+	mi.Total += other.Total
+}
+
+func (mi *MedalsInfo) Less(other MedalsInfo) bool {
+	return mi.Gold < other.Gold ||
+		mi.Gold == other.Gold && mi.Silver < other.Silver ||
+		mi.Gold == other.Gold && mi.Silver == other.Silver && mi.Bronze < other.Bronze
+}
+
+type AthleteData struct {
+	Name    string `json:"athlete"`
+	Age     int
+	Country string
+	Year    int
+	Date    string
+	Sport   string
+	MedalsInfo
+}
+
+type Data []AthleteData
+
+var data Data
+
+func ParseInputData(path string) {
+	jsonData, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ParseArgs() (string, string) {
+	port := flag.String("port", "8080", "port")
+	dataPath := flag.String("data", "./olympics/testdata/olympicWinners.json", "path to json with data")
+	flag.Parse()
+	return *port, *dataPath
+}
+
+type AthleteInfoResult struct {
+	Name         string             `json:"athlete"`
+	Country      string             `json:"country"`
+	Medals       MedalsInfo         `json:"medals"`
+	MedalsByYear map[int]MedalsInfo `json:"medals_by_year"`
+}
+
+func GetAthleteInfoByNameAndSport(name string, sport *string) (*AthleteInfoResult, error) {
+	info := make([]*AthleteData, 0)
+	for _, a := range data {
+		if a.Name == name && (sport == nil || a.Sport == *sport) {
+			info = append(info, &a)
+		}
+	}
+
+	if len(info) == 0 {
+		return nil, errors.New("Not found")
+	}
+
+	result := AthleteInfoResult{
+		Name:         name,
+		Country:      info[0].Country,
+		MedalsByYear: make(map[int]MedalsInfo),
+	}
+	for _, infoByYear := range info {
+		result.Medals.Add(infoByYear.MedalsInfo)
+		result.MedalsByYear[infoByYear.Year] = infoByYear.MedalsInfo
+	}
+
+	return &result, nil
+}
+
+func AthleteInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	resultData, err := GetAthleteInfoByNameAndSport(name, nil)
+	if err != nil {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	result, _ := json.Marshal(*resultData)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(result)
+}
+
+func TopAthletesInSport(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	sport := r.URL.Query().Get("sport")
+	athletes := make(map[string]MedalsInfo)
+	for _, a := range data {
+		if a.Sport == sport {
+			m := athletes[a.Name]
+			m.Add(a.MedalsInfo)
+			athletes[a.Name] = m
+		}
+	}
+
+	if len(athletes) == 0 {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	medalsByName := make([]struct {
+		name   string
+		medals MedalsInfo
+	}, 0)
+	for name, medals := range athletes {
+		medalsByName = append(medalsByName, struct {
+			name   string
+			medals MedalsInfo
+		}{name: name, medals: medals})
+	}
+	sort.Slice(medalsByName, func(i, j int) bool {
+		return medalsByName[j].medals.Less(medalsByName[i].medals) ||
+			medalsByName[i].medals == medalsByName[j].medals && medalsByName[i].name < medalsByName[j].name
+	})
+
+	k := 3
+	if r.URL.Query().Has("limit") {
+		var err error
+		k, err = strconv.Atoi(r.URL.Query().Get("limit"))
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+		}
+	}
+	medalsByName = medalsByName[:k]
+
+	resultData := make([]AthleteInfoResult, 0)
+	for _, a := range medalsByName {
+		info, _ := GetAthleteInfoByNameAndSport(a.name, &sport)
+		resultData = append(resultData, *info)
+	}
+
+	result, _ := json.Marshal(resultData)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(result)
+}
+
+func TopCountriesInYear(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	year, _ := strconv.Atoi(r.URL.Query().Get("year"))
+	countries := make(map[string]MedalsInfo)
+	for _, a := range data {
+		if a.Year == year {
+			m := countries[a.Country]
+			m.Add(a.MedalsInfo)
+			countries[a.Country] = m
+		}
+	}
+
+	if len(countries) == 0 {
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
+
+	medalsByCountry := make([]struct {
+		Country string `json:"country"`
+		MedalsInfo
+	}, 0)
+	for name, medals := range countries {
+		medalsByCountry = append(medalsByCountry, struct {
+			Country string `json:"country"`
+			MedalsInfo
+		}{Country: name, MedalsInfo: medals})
+	}
+	sort.Slice(medalsByCountry, func(i, j int) bool {
+		return medalsByCountry[j].MedalsInfo.Less(medalsByCountry[i].MedalsInfo) ||
+			medalsByCountry[i].MedalsInfo == medalsByCountry[j].MedalsInfo && medalsByCountry[i].Country < medalsByCountry[j].Country
+	})
+
+	k := 3
+	if r.URL.Query().Has("limit") {
+		var err error
+		k, err = strconv.Atoi(r.URL.Query().Get("limit"))
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+		}
+	}
+	medalsByCountry = medalsByCountry[:min(k, len(medalsByCountry))]
+
+	result, _ := json.Marshal(medalsByCountry)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(result)
+
+}
+
+func main() {
+	port, path := ParseArgs()
+	ParseInputData(path)
+
+	http.HandleFunc("/athlete-info", AthleteInfo)
+	http.HandleFunc("/top-athletes-in-sport", TopAthletesInSport)
+	http.HandleFunc("/top-countries-in-year", TopCountriesInYear)
+	log.Fatal(http.ListenAndServe("localhost:"+port, nil))
 }
